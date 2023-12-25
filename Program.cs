775181f331f528da1fdbaad95dded95a5deb0ad5
glsaacke <glsaacke@crimson.ua.edu>
2023-12-25 14:49:31 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//MAIN***
+
+System.Console.WriteLine("Welcome to tic-tac-toe!\n\nHit any key to start a new game");
+Console.ReadKey();
+
+DisplayBoard();
+
+//***END
+
+static void DisplayBoard(){
+    Console.Clear();
+    System.Console.WriteLine("___|___|___");
+    System.Console.WriteLine("___|___|___");
+    System.Console.WriteLine("   |   |   ");
+}
